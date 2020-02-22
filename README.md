6a0192e1cce777f3c4abaa8b0b1752ffe40d427a
Gabriele Modena <gm@nowave.it>
2020-02-22 23:57:05 +0100

    Add custom target def for bare metal cortex

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # binaryno
 Rust binary running on bare metal arm cortex m4.
 
+# Install deps & build
+
+```
+$ cargo install cargo-xbuild
+$ cargo install cargo-binutils
+$ rustup default nightly
+$ rustup target add thumbv7em-none-eabihf
+$ rustup component add rust-src
+$ rustup component add llvm-tools-preview
+$ cargo xbuild --release
+
+```
+
+# Run
+
 # Resources
 
 - https://os.phil-opp.com/freestanding-rust-binary/
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![no_std]
 #![no_main]
 
-
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"sic transit gloria mundi";
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -13,5 +14,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    
     loop {}
 }
